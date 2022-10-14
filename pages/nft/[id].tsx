2fa0c29ef1d4d641de3625d1615bbf3d4a33ad54
Shamik1836 <shamikdas2001@gmail.com>
2022-10-14 21:41:13 +0530

    Added the Sanity Backend, created the first NFT Drop Collection

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { ConnectWallet, useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
-function NFTDropPage() {
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from '../../sanity';
+import { Collection } from '../../typings';
+import Link from 'next/link';
+
+interface Props{
+    collection: Collection
+}
+function NFTDropPage({collection}) {
     const connectWithMetamask=useMetamask();
     const address=useAddress();
     const disconnect=useDisconnect();
@@ -11,15 +19,15 @@ function NFTDropPage() {
         <div className='bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4'>
             <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
                 <div className='bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl'>
-                <img src="https://links.papareact.com/8sg" alt="" className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'/>
+                <img src={urlFor(collection.previewImage).url()} alt="" className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'/>
                 </div>
                 
                 <div className='text-center p-5 space-y-2'>
                     <h1 className='text-4xl font-bold text-white'>
-                        PAPAFAM Apes
+                        {collection.nftCollectionName}
                     </h1>
                     <h2 className='text-xl text-gray-300'>
-                        A collection of PAPAFAM Apes who live & breathe React!
+                        {collection.description}
                     </h2>
                 </div>
             </div>
@@ -28,7 +36,10 @@ function NFTDropPage() {
         <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
             {/* Header */}
                 <header className='flex items-center justify-between'>
+                    <Link href={'/'}>
                     <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>The <span className='font-extrabold underline decoration-pink-600/50'> PAPAFAM </span>{' '} NFT Market Place</h1>
+                    </Link>
+
                     <button onClick={()=>(address?disconnect():connectWithMetamask())}className='rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base'>
                         {address ? 'Sign Out':'Sign In'}
                     </button>
@@ -40,9 +51,9 @@ function NFTDropPage() {
                 )}
             {/* Content */}
             <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
-                <img src="https://links.papareact.com/bdy" alt="" className='w-80 object-cover pb-10 lg:h-40'/>
+                <img src={urlFor(collection.mainImage).url()} alt="" className='w-80 object-cover pb-10 lg:h-40'/>
                 <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>
-                    The PAPAFAM Ape Coding Club | NFT Drop
+                    {collection.title}
                 </h1>
                 <p className='pt-2 text-xl text-green-500'>13 / 21 NFT's claimed</p>
             </div>
@@ -56,4 +67,47 @@ function NFTDropPage() {
   )
 }
 
-export default NFTDropPage
+export default NFTDropPage;
+
+export const getServerSideProps:GetServerSideProps=async({params})=>{
+    const query=`*[_type=="collection" && slug.current==$id][0]{
+        _id,
+        title,
+        address,
+        description,
+        nftCollectionName,
+        mainImage {
+            asset
+        },
+        previewImage {
+            asset
+        },
+        slug {
+            current
+        },
+        creator-> {
+            _id,
+            name,
+            address,
+            slug {
+                current
+            }
+        }
+    }`
+
+    const collection=await sanityClient.fetch(query,{
+        id: params?.id
+    })
+
+    if (!collection){
+        return {
+            // This would return a 404 page if the page doesn't exist
+            notFound: true
+        }
+    }
+    return {
+        props:{
+            collection
+        }
+    }
+}
